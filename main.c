bc43dcf10141e79526175005e017839b74603519
Alex Jordan <alex@strugee.net>
2017-02-09 15:39:17 -0500

    Properly handle EOF inputs

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 enum oper_types { OPER_ADD, OPER_SUB, OPER_MULT, OPER_DIV };
 
 int get_oper_type()
 {
+	fpurge(stdin);
+
 	printf("What kind of operation would you like to perform? [asmd] ");
 
 	char oper_type[3];
-	fgets(oper_type, 3, stdin);
+	
+
+	if (fgets(oper_type, 3, stdin) == NULL) {
+		if (feof(stdin) != 0) {
+			printf("\n");
+			exit(0);
+		} else {
+			/* We encountered an error */
+			printf("\n");
+			perror("icalc");
+			exit(1);
+		}
+	}
 
 	switch (oper_type[0]) {
 	case 'a':
@@ -19,6 +35,7 @@ int get_oper_type()
 	case 'd':
 		return OPER_DIV;
 	default:
+		
 		return get_oper_type();
 	}
 }
@@ -28,6 +45,7 @@ int main()
 	int first, second;
 
 	int oper = get_oper_type();
+	fpurge(stdin);
 	printf("%i\n", oper);
 
 	return 0;
